10b178d6e8dc470b64228ffa44717e331a403380
Giovanna Cunha <giovannakelli@gmail.com>
2021-09-23 23:28:53 -0300

    addTask page was created

--- src/pages/NewTask/index.js
+++ src/pages/NewTask/index.js
@@ -1,13 +1,35 @@
 import React, {useState, useEffect} from 'react'
-import {Text, SafeAreaView} from 'react-native'
+import {Text, View, TextInput, TouchableOpacity} from 'react-native'
+import database from '../../config/firebaseconfig';
+import {styles} from './styles'
 
+export const NewTask = ({navigation}) => {
+  const [description, setDescription] = useState(null);
 
-export const NewTask = () => {
+  const addTask = () => {
+    database.collection("Tasks").add({
+      description: description,
+      status: false,
+    })
+    navigation.navigate("Task")
+  }
+
   return (
-    <SafeAreaView>
-      <Text>
-        New Task
-      </Text>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Ex: Fazer Café..."
+        onChangeText={setDescription}
+        value={description}
+      />
+      <TouchableOpacity
+        style={styles.buttonNewTask}
+        onPress={() => {
+          addTask()
+        }}
+      >
+        <Text style={styles.iconButton}>Salvar</Text>
+      </TouchableOpacity>
+    </View>
   )
 }
